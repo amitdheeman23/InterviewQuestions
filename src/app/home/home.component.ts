636edd7b2e093d909b51e1fd1c1baaffa0ea1b20
Amit Kumar <amit.kumar@greychaindesign.com>
2023-09-29 13:09:18 +0530

    Updated code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-// @Component({
-//   selector: 'app-home',
-//   templateUrl: './home.component.html',
-//   styleUrls: ['./home.component.scss']
-// })  This is user with selector 
-
-// @Component({
-//   selector: '.app-home',
-//   templateUrl: './home.component.html',
-//   styleUrls: ['./home.component.scss']
-// }) This used with class 
-
-// @Component({
-//   selector: '[apphome]',
-//   templateUrl: './home.component.html',
-//   styleUrls: ['./home.component.scss']
-// }) Used By Array
-
 @Component({
-  selector: '[apphome]',
+  selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
