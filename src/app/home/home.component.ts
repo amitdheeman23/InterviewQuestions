58bb480e6855409ebf92af046378225d20f47ed9
Amit Kumar <amit.kumar@greychaindesign.com>
2023-09-29 16:29:18 +0530

    Encapsulation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,33 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { Employee } from '../employee';
-
-class Employee2{
-  constructor(){
-
-  }
-
-  ok(){
-    alert("Class is called")
-  }
-}
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  viewProviders:[Employee,Employee2],
-  preserveWhitespaces:true//User For preserveWhitespaces html elements mai bich space kai liye
+  encapsulation:ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
 
-
-  constructor(private obj:Employee,private obj2:Employee2) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.obj.show();
-    this.obj2.ok();
   }
-
-
 
 }
