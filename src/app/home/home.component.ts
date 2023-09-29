fc5c0309131b1cd71f2f8196bfba36befc7990cf
Amit Kumar <amit.kumar@greychaindesign.com>
2023-09-29 16:54:43 +0530

    Input property se parent se data child pr send kr skte hai

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,10 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  inputs:['data']
 })
 export class HomeComponent implements OnInit {
-
+  data:any;
   constructor() { }
 
   ngOnInit(): void {
