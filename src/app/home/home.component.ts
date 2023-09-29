72fbbd4230d44bf0bc538027ded65bc234211cec
Amit Kumar <amit.kumar@greychaindesign.com>
2023-09-29 13:37:21 +0530

    style and styleurl

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  // templateUrl: './home.component.html',
-  template:`<h1>Hello</h1>
-  Amit`,
-  styleUrls: ['./home.component.scss']
+  templateUrl: './home.component.html',
+  // styleUrls: ['./home.component.scss']
+  styles:[`h1{background-color:red}`]
 })
 export class HomeComponent implements OnInit {
 
