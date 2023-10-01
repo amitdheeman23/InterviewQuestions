08e1b414412b4d81d8cfad52e3e7732af4350890
Amit Kumar <amit.kumar@greychaindesign.com>
2023-10-01 13:58:43 +0530

    ngFor Grouping

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,49 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  items = [
-    { id: 1, name: 'Item 1' },
-    { id: 2, name: 'Item 2' },
-    { id: 3, name: 'Item 3' },
+  counteryDetails:any[]=[
+    {
+      'country':'India',
+      'people':[{
+        'name':'Amit Kumar',
+      },{
+        'name':'Mohan Kumar',
+      }]
+    },
+    {
+      'country':'UK',
+      'people':[{
+        'name':'Sohan Kumar',
+      },{
+        'name':'Pankaj Kumar',
+      }]
+    }
   ];
-
-  // trackByFunction(index: number, item: any): number {
-  //   return item.id; // Use a unique identifier, such as item's ID
-  // }
-  students:any[]=[]
-
-
-  constructor() {
-    this.students=[
-      {
-        'name':"Rahul",id:1
-      },   {
-        'name':"Ajeet",id:2
-      },   {
-        'name':"Rohan",id:3
-      },   {
-        'name':"Mahesh",id:4
-      },   {
-        'name':"Pankaj",id:5
-      },
-    ];
-  
-   }
+  constructor() {}
 
   ngOnInit(): void {
   }
 
-  trackByFunction(index:number,item:any):string{
-    // console.log('line---',d);
-    
-    return item.id
-  }
-
-  clickOn(){
-    this.items.push({
-      'name':"Kamal",id:6
-    })
-  }
 
 
 }
