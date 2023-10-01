758bb3ce55d36cad766602f72f85a48d4a1ec476
Amit Kumar <amit.kumar@greychaindesign.com>
2023-10-01 13:49:37 +0530

    ngFor with trackBy

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,14 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  items = [
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' },
+    { id: 3, name: 'Item 3' },
+  ];
 
-  name="Sohan"
+  // trackByFunction(index: number, item: any): number {
+  //   return item.id; // Use a unique identifier, such as item's ID
+  // }
+  students:any[]=[]
 
-  constructor() { }
+
+  constructor() {
+    this.students=[
+      {
+        'name':"Rahul",id:1
+      },   {
+        'name':"Ajeet",id:2
+      },   {
+        'name':"Rohan",id:3
+      },   {
+        'name':"Mahesh",id:4
+      },   {
+        'name':"Pankaj",id:5
+      },
+    ];
+  
+   }
 
   ngOnInit(): void {
   }
 
+  trackByFunction(index:number,item:any):string{
+    // console.log('line---',d);
+    
+    return item.id
+  }
+
+  clickOn(){
+    this.items.push({
+      'name':"Kamal",id:6
+    })
+  }
 
 
 }
