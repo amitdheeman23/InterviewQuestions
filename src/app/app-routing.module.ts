feb0af49ac142a93c9e73ca0c47c6a6e11177e53
Amit Kumar <amit.kumar@greychaindesign.com>
2023-10-04 22:32:41 +0530

    Template-driven forms and Model-driven forms (Reactive forms)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ParentComponent } from './parent/parent.component';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+
 
 
 const routes: Routes = [
-  {path:'',redirectTo:'home',pathMatch:'full'},
-  {path:'home',component:HomeComponent},
-  // {path:'parent',component:ParentComponent},
-  // {path:'**',component:PagenotfoundComponent},
-  {path:'feature-1',loadChildren:()=>import('./fraturemodule1/one/one.module').then(m=>m.OneModule)},
-  {path:'feature-2',loadChildren:()=>import('./fraturemodule2/two/two.module').then(m=>m.TwoModule)},
+
+
+  // {path:'feature-1',loadChildren:()=>import('./fraturemodule1/one/one.module').then(m=>m.OneModule)},
+  // {path:'feature-2',loadChildren:()=>import('./fraturemodule2/two/two.module').then(m=>m.TwoModule)},
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ParentComponent } from './parent/parent.component';
-import { HomeComponent } from './home/home.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
-import { TwoComponent } from './fraturemodule2/two/two.component';
 import { Data3Service } from './data3.service';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ParentComponent,
     HomeComponent,
-    PagenotfoundComponent,
-    TwoComponent,
   ],
   imports: [
     BrowserModule, 
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
-
-
+    ReactiveFormsModule
   ],
   providers: [Data3Service],
   bootstrap: [AppComponent]
