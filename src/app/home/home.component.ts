78ff2d33aa9ae904d548e97f75a505b6aa6f4924
Amit Kumar <amit.kumar@greychaindesign.com>
2023-10-10 21:06:40 +0530

    valueChanges

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  form!:FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.form=new FormGroup({
+      name:new FormControl('')
+    });
+
+    this.form.get('name')?.valueChanges.subscribe((res)=>{
+      console.log(res);
+      
+    })
   }
+
+
+
+
  
 }
